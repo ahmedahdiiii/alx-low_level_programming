e8dae0f2ad61316dbcde363376e21da258612c07
ahmedahdiiii <ahdiahmed245@gmail.com>
2023-04-12 09:50:47 -0700

    libary

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * _strlen - changes the initial.
+  * @s: parameter to change.
+  * Return: len
+  */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
